d8080c1d40ac7b0748b8970626f4ab1f62f1de8e
greeyash <ekanabila224@gmail.com>
2026-08-07 21:43:41 +0700

    fix: dashboard

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,20 @@
+import LocationCard from "@/components/LocationCard";
+import FeedsCarousel from "@/components/FeedsCarousel";
+import CategoryGrid from "@/components/CategoryGrid";
+import HelpCenterCard from "@/components/HelpCenterCard";
+
+export default function DashboardPage() {
+  return (
+    <main className="mx-auto max-w-5xl px-4 pt-3 pb-6 sm:px-6 sm:pt-6 lg:px-8 flex flex-col gap-5 sm:gap-6">
+      <div className="w-full">
+        <LocationCard />
+      </div>
+
+      <FeedsCarousel />
+
+      <CategoryGrid />
+
+      <HelpCenterCard />
+    </main>
+  );
+}
