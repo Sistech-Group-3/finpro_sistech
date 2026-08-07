e881754aa6abf447c7ada3c9d5f11b2577596c37
Sinta Bella <sb244758@gmail.com>
2026-08-08 00:54:50 +0700

    Fixing layout and feature settings

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,4 @@
+import Navbar from "@/components/Navbar";
 import LocationCard from "@/components/LocationCard";
 import FeedsCarousel from "@/components/FeedsCarousel";
 import CategoryGrid from "@/components/CategoryGrid";
@@ -5,16 +6,23 @@ import HelpCenterCard from "@/components/HelpCenterCard";
 
 export default function DashboardPage() {
   return (
-    <main className="mx-auto max-w-5xl px-4 pt-3 pb-6 sm:px-6 sm:pt-6 lg:px-8 flex flex-col gap-5 sm:gap-6">
-      <div className="w-full">
-        <LocationCard />
-      </div>
+    <div className="min-h-screen w-full bg-[#FBD9EC]">
+      {/* Navbar */}
+      <header className="fixed inset-x-0 top-0 z-[9999]">
+        <Navbar />
+      </header>
 
-      <FeedsCarousel />
-
-      <CategoryGrid />
-
-      <HelpCenterCard />
-    </main>
+      {/* Page */}
+      <main className="w-full flex justify-center pt-20 pb-10">
+        <div className="w-full max-w-[430px]">
+          <div className="px-5 space-y-4">
+            <LocationCard />
+            <FeedsCarousel />
+            <CategoryGrid />
+            <HelpCenterCard />
+          </div>
+        </div>
+      </main>
+    </div>
   );
 }
--- app/report/page.tsx
+++ app/report/page.tsx
@@ -1,20 +1,31 @@
+import Navbar from "@/components/Navbar";
 import LocationContext from "@/components/report/LocationContext";
 import FileUpload from "@/components/report/FileUpload";
 import ReportForm from "@/components/report/ReportForm";
 
 export default function ReportPage() {
   return (
-    <div className="mx-auto max-w-5xl space-y-6 pb-12">
-      <div className="grid grid-cols-1 gap-6 lg:grid-cols-12">
-        <div className="space-y-6 lg:col-span-5">
-          <LocationContext />
-          <FileUpload />
-        </div>
+    <div className="min-h-screen w-full bg-[#FBD9EC]">
+      {/* Navbar */}
+      <header className="fixed inset-x-0 top-0 z-[9999]">
+        <Navbar />
+      </header>
 
-        <div className="lg:col-span-7">
-          <ReportForm />
+      {/* Page */}
+      <main className="w-full flex justify-center pt-20 pb-10">
+        <div className="w-full max-w-[430px] lg:max-w-5xl px-5">
+          <div className="grid grid-cols-1 gap-5 lg:grid-cols-12">
+            <div className="space-y-5 lg:col-span-5">
+              <LocationContext />
+              <FileUpload />
+            </div>
+
+            <div className="lg:col-span-7">
+              <ReportForm />
+            </div>
+          </div>
         </div>
-      </div>
+      </main>
     </div>
   );
 }
