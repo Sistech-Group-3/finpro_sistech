75438c2e0943205e87d3c76db023f5a1c298bf19
Sinta Bella <sb244758@gmail.com>
2026-08-07 22:13:07 +0700

    Fix Navbar

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -6,14 +6,9 @@ import HelpCenterCard from "@/components/HelpCenterCard";
 export default function DashboardPage() {
   return (
     <main className="mx-auto max-w-5xl px-4 pt-3 pb-6 sm:px-6 sm:pt-6 lg:px-8 flex flex-col gap-5 sm:gap-6">
-      <div className="w-full">
-        <LocationCard />
-      </div>
-
+      <LocationCard />
       <FeedsCarousel />
-
       <CategoryGrid />
-
       <HelpCenterCard />
     </main>
   );
